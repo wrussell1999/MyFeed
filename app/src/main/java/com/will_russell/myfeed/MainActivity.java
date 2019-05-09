77234ce45c5594c3902973347169a6c2fce1582f
Will Russell <will@will-russell.com>
2019-05-09 18:21:43 +0100

    UI Tweaks and colours

--- app/src/main/java/com/will_russell/myfeed/MainActivity.java
+++ app/src/main/java/com/will_russell/myfeed/MainActivity.java
@@ -23,6 +23,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.BasicNetwork;
 import com.android.volley.toolbox.DiskBasedCache;
 import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.StringRequest;
 
 import java.io.InputStream;
